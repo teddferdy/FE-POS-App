47c349f5db97b74c7fa1bfc6aa10676c996ffa51
Teddy-FN <tedd.ferdy@gmail.com>
2024-07-27 21:53:02 +0700

    Add Loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { LoadingProvider } from "./components/UI/loading";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <LoadingProvider>
+        <App />
+      </LoadingProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
---
+++ src/services/auth.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const login = async (payload) => {
+  const { data, status } = await axios.post(
+    `https://api-bisa-nota.vercel.app/auth/login`,
+    payload,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (status !== 200) throw Error(`Error`);
+
+  return data;
+};
+
+export const register = (payload) => {};
+
+export const resetPassword = (payload) => {};
