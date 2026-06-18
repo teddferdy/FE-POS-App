a2eab90684fb1694e4e6d71798ed843923515f88
Teddy-FN <teddy.amrullah@ist.id>
2026-06-18 21:30:20 +0700

    fix: revert i18n to static imports to restore translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -3,6 +3,11 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+// Import Language
+import translationEn from "./en.json";
+import translationJpn from "./jpn.json";
+import translationId from "./id.json";
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -12,7 +17,19 @@ i18n
     fallbackLng: "en",
     load: "languageOnly",
     supportedLngs: ["en", "id", "jpn"],
+    preload: ["en", "id", "jpn"],
     keySeparator: false,
     interpolation: { escapeValue: false },
+    resources: {
+      en: {
+        translation: translationEn
+      },
+      jpn: {
+        translation: translationJpn
+      },
+      id: {
+        translation: translationId
+      }
+    }
   });
 export default i18n;
