91aaae1ae49ea7c48a2e76ce94e83778766b1616
Teddy-FN <teddy.amrullah@ist.id>
2026-09-01 22:54:51 +0700

    feat(report): add report layout settings page

--- src/services/reportConfig.js
+++ src/services/reportConfig.js
@@ -17,3 +17,9 @@ export const saveReportConfig = async (key, config) => {
   if (status !== 200 && status !== 201) throw Error(`${data?.message}`);
   return data;
 };
+
+export const getReportConfigMeta = async () => {
+  const { data, status } = await axiosInstance.get("/report-config/meta");
+  if (status !== 200) throw Error(`${data?.message}`);
+  return data;
+};
